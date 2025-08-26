8b22b3253d7f6edfd01d0aae58e9aa0560b31990
dmsRosa6 <dms.rosa6@gmail.com>
2025-08-26 16:05:31 +0000

    started the save state

--- internal/session/session.go
+++ internal/session/session.go
@@ -1,6 +1,8 @@
 package session
 
 import (
+	"encoding/json"
+	"os"
 	"time"
 
 	bt "github.com/dmsosa6/bittorrent-client/internal/bittorrent"
@@ -40,4 +42,71 @@ func (s *Session) Run() {
 			}
 		}
 	}
+}
+
+type PersistedTorrent struct {
+	InfoHash    bt.InfoHash `json:"info_hash"`
+	Downloaded  int64       `json:"downloaded"`
+	TotalLength int64       `json:"total_length"`
+	SavePath    string      `json:"save_path"`
+	Bitfield    []bool      `json:"bitfield"`
+}
+
+type PersistedSession struct {
+	Torrents    []PersistedTorrent `json:"torrents"`
+	CurrTorrent bt.InfoHash        `json:"curr_torrent"`
+}
+
+func (s *Session) Save(path string) error {
+	persisted := PersistedSession{}
+
+	for _, t := range s.Torrents {
+		pt := PersistedTorrent{
+			InfoHash:    t.InfoHash,
+			Downloaded:  t.BytesDownloaded,
+			TotalLength: t.TotalLength,
+			SavePath:    t.SavePath,
+			Bitfield:    t.Bitfield,
+		}
+		persisted.Torrents = append(persisted.Torrents, pt)
+	}
+
+	if s.CurrTorrent != nil {
+		persisted.CurrTorrent = s.CurrTorrent.InfoHash
+	}
+
+	data, err := json.MarshalIndent(persisted, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	tmp := path + ".tmp"
+	if err := os.WriteFile(tmp, data, 0644); err != nil {
+		return err
+	}
+	return os.Rename(tmp, path)
+}
+
+func (s *Session) Load(path string) error {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	var persisted PersistedSession
+	if err := json.Unmarshal(data, &persisted); err != nil {
+		return err
+	}
+
+	for _, pt := range persisted.Torrents {
+		//need a way to reconstruct a Torrent
+		t := bt.NewTorrentFromState(pt.InfoHash, pt.SavePath, pt.TotalLength, pt.Bitfield)
+		s.Torrents[t.InfoHash] = t
+
+		if pt.InfoHash == persisted.CurrTorrent {
+			s.CurrTorrent = t
+		}
+	}
+
+	return nil
 }
